d182c4513ae2fac82f10fb91cc7fb74ea3c4d207
Ernesto Vargas <netovargas90@gmail.com>
2018-07-31 11:46:45 -0500

    Add preview to articles, combine music+podcast buttons

--- omc2-lambda/onemancult.py
+++ omc2-lambda/onemancult.py
@@ -12,26 +12,37 @@
 #  Amazon S3 file is just a container for reading the static
 #  text file with all the info.
 
-import requests
-import time
-import json
 import config
-import boto3
+import json
 import sys
+import time
+from os import name, system, path
+
+import boto3
+import requests
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def clear():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
 
 def main():
-	prompt = """
-	Select the mode you want to run by entering the number
+	prompt = """Select the mode you want to run by entering the number
 	associated with each option below.
-		1 - Update Airtable from Mercury API
-		2 - Regenerate JSON file from Airtable
-		3 - Submit JSON file to One Man Cult website (AWS)
-		4 - *Update Airtable, generate JSON file and submit
-			to AWS (full workflow)*
-		5 - Submit email to Mailchimp subscribers
-		p - Print options again
-		q - Quit
-	"""
+	1 - Update Airtable from Mercury API
+	2 - Regenerate JSON file from Airtable
+	3 - Submit JSON file to One Man Cult website (AWS)
+	4 - *Update Airtable, generate JSON file and submit
+		to AWS (full workflow)*
+	5 - Submit email to Mailchimp subscribers
+	p - Print options again
+	q - Quit"""
 	print (prompt)
 
 	response = -1
@@ -43,11 +54,13 @@ def main():
 			# running all articles that are missing data through the
 			# Mercury API.
 			airtableupdate()
+			print ('\n\n' + prompt)
 		elif (response == '2'):
 			# Generates a JSON file with the article data from Airtable
 			# and saves it to the same directory as this file. Does not
 			# submit the file to the S3 bucket.
 			airtableJsonOutput()
+			print ('\n\n' + prompt)
 		elif (response == '3'):
 			# Note: This option assumes the filename is articledata.json
 			# and is in the directory of this file.
@@ -56,6 +69,7 @@ def main():
 			# S3 bucket.
 			filename = 'articledata.json'
 			uploadToS3(filename)
+			print ('\n\n' + prompt)
 		elif (response == '4'):
 			# This option will update the Airtable base using Mercury,
 			# generate a JSON file from it and then submit that file to
@@ -64,14 +78,15 @@ def main():
 			airtableupdate()
 			filename = airtableJsonOutput()
 			uploadToS3(filename)
+			print ('\n\n' + prompt)
 		elif (response == '5'):
-			print "This option has not been implemented yet. You can choose another one."
+			print ("This option has not been implemented yet. You can choose another one.")
 		elif (response == 'q'):
 			exit()
 		elif (response == 'p'):
-			print prompt
+			print (prompt)
 		else:
-			print "Invalid response. Please enter another option from the ones above."
+			print ("Invalid response. Please enter another option from the ones above.")
 
 def main2():
 	airtableupdate()
@@ -79,13 +94,16 @@ def main2():
 	uploadToS3(filename)
 
 def uploadToS3(filename):
+	print ("\nCreating connection to AWS S3 (www.onemancult.co).")
+	filename_path = path.dirname(path.realpath(__file__)) + '/' + filename
 	s3 = boto3.client('s3',
          aws_access_key_id = config.aws_access_key_id,
          aws_secret_access_key = config.aws_secret_access_key)
 
 	bucketname = 'www.onemancult.co'
 
-	s3.upload_file(filename, bucketname, filename)
+	s3.upload_file(filename_path, bucketname, filename)
+	print ("JSON file uploaded to S3 bucket.")
     	
 def airtableupdate():
 	# Set up Airtable headers, API call URL and paramters
@@ -97,6 +115,7 @@ def airtableupdate():
 	parameter = "?filterByFormula=Posted%3D0&maxRecords=" + str(recordCount) + "&sort%5B0%5D%5Bfield%5D=Created+Time&sort%5B0%5D%5Bdirection%5D=desc"
 
 	# Make API get request to get URLs of entries not posted
+	print ("Fetching URLs of unposted links.")
 	r = requests.get(baseApiUrl + parameter, headers=headers)
 	rjson = r.json()
 
@@ -110,16 +129,24 @@ def airtableupdate():
 
 	# Iterate through URLs from GET request
 	dictLength = len(rjson['records'])
+	print ("Updating Airtable with link information.")
 	for indexNum in range (0, dictLength, 1):
 		recordUrl = rjson['records'][indexNum]['fields']['Url']
 		type = rjson['records'][indexNum]['fields']['Type']
 		# Pass URL to Mercury API to get link information
 		fieldDict = mercurycall(recordUrl)
 		# Set up API PUT request to update table
+		posted = 0
+		if ('Title' in fieldDict.keys()):
+			# Only mark links as posted if we were able to get a
+			# title.			
+			posted = 1
+		else:
+			fieldDict['Title'] = "Error - Needs manual edit."
 		fieldDict.update(
 		{
 			"Url": rjson['records'][indexNum]['fields']['Url'],
-			"Posted": 1,
+			"Posted": posted,
 			"Type": type
 		})
 		payload = {
@@ -132,17 +159,18 @@ def airtableupdate():
 			headers=headers,
 			data=json.dumps(payload)
 			)
-		# Print response in case something went wrong
-		#print r
-		# API has limit of 5 calls per second, wait so it doesn't
-		# time out.
-		#print "Updated: " + fieldDict.Title
+		print (" URL: " + str(fieldDict['Url'])[0:49] + "... - " + fieldDict['Type'])
+		print (" Title: " + fieldDict['Title'])
+		print (" Reading Time: " + str(fieldDict['Reading Time']))
+		print (" Excerpt: " + str(fieldDict['Excerpt'])[0:64] + "...")
+		print ("")
 		time.sleep(.25)
 
 def airtableJsonOutput():
-	# Create and open csv file and write header
-	filename = 'articledata.json'
-	jsonFile = open(filename, "wb")
+	# Create and open json file and write header
+	filename = path.dirname(path.realpath(__file__)) + '/articledata.json'
+	jsonFile = open(filename, 'wb')
+	print ("Updating JSON file from Airtable.")
 
 	# Set up Airtable headers and API call URL
 	headers = {
@@ -157,9 +185,10 @@ def airtableJsonOutput():
 	#Max Records = 60
 	#Sort = By Created Time, descending
 	# URL Encoder - https://codepen.io/airtable/full/rLKkYB?baseId=appleEv8lPCl3jhFw&tableId=tblFbKbLEGWoqe3R3
-	parameter = "?fields%5B%5D=Url&fields%5B%5D=Title&fields%5B%5D=Reading+Time&fields%5B%5D=Created+Time&fields%5B%5D=Type&filterByFormula=Posted+%3D+1&maxRecords=60&sort%5B0%5D%5Bfield%5D=Created+Time&sort%5B0%5D%5Bdirection%5D=desc"
+	parameter = "?fields%5B%5D=Url&fields%5B%5D=Title&fields%5B%5D=Reading+Time&fields%5B%5D=Created+Time&fields%5B%5D=Type&fields%5B%5D=Excerpt&filterByFormula=Posted+%3D+1&maxRecords=60&sort%5B0%5D%5Bfield%5D=Created+Time&sort%5B0%5D%5Bdirection%5D=desc"
 
 	# Make request
+	print ("Requesting most recent data from Airtable.")
 	r = requests.get(baseApiUrl + parameter, headers=headers)
 	rjson = r.json()
 	dictLength = len(rjson['records'])
@@ -167,24 +196,27 @@ def airtableJsonOutput():
 
 	# Iterate through response and save each line to JSON
 	for indexNum in range (0, dictLength, 1):
-		#fieldCount = len(rjson['records'][indexNum]['fields'])
+		print (str(indexNum+1) + "..."),
 		title = rjson['records'][indexNum]['fields']['Title']
 		url = rjson['records'][indexNum]['fields']['Url']
 		readTime = rjson['records'][indexNum]['fields']['Reading Time']
 		type = rjson['records'][indexNum]['fields']['Type']
 		createdTime = rjson['records'][indexNum]['fields']['Created Time']
+		excerpt = rjson['records'][indexNum]['fields']['Excerpt']
 		outputDict.update({
 			indexNum: {
 				"title": title,
 				"url": url,
 				"readTime": readTime,
 				"type": type,
-				"createdTime": createdTime
+				"createdTime": createdTime,
+				"excerpt": excerpt
 			}
 			})
 
-	print json.dump(outputDict,jsonFile)
-	#jsonFile.write()
+	#print json.dump(outputDict,jsonFile)
+	json.dump(outputDict, jsonFile)
+	#jsonFile.write(json.dump(outputDict, jsonFile))
 	jsonFile.close()
 
 	return filename
@@ -220,4 +252,3 @@ def mercurycall(airtableUrl):
 
 if __name__ == "__main__":
 	main()
-
--- omc2/script.js
+++ omc2/script.js
@@ -19,16 +19,31 @@ function loadFilter(filterType)
 {
   if (filterType == 'all') {
     $(".grid-item").css( "display", "inline" );
+  } else if (filterType == 'listen') {
+    $(".grid-item").css( "display", "none" );
+    $(".music").css( "display", "inline");
+    $(".podcast").css( "display", "inline");
   } else {
     $(".grid-item").css( "display", "none" );
     $("."+filterType).css( "display", "inline");
   }
 }
 
+function loadPreview()
+{
+  $(".excerpt").toggle();
+}
+
+function trimEllipsis(text)
+{
+  newtext = text.replace('&hellip;', '…')
+  return newtext
+}
+
 $(document).ready(function(){
     var jsondata;
     $.getJSON("articledata.json", function(data) {
-      console.log("success");
+      console.log("File loaded successfully.");
       jsondata = data;
       
       var articleContainer = $(".grid-container");
@@ -46,12 +61,17 @@ $(document).ready(function(){
           console.log(lastFriday - createdDate);
           headline.prepend("<sup>new</sup>");
         }
-        var readTime;
+        var readTime = "";
+        var excerpt = "";
         if (jsondata[i]["type"] == "read") {
           if (jsondata[i]["readTime"] == -1) {
             readTime = $("<p></p>").text("Article");
+            //excerpt = $("<p></p>").text(jsondata[i]["excerpt"]).addClass("excerpt");
+            excerpt = $("<p></p>").text(trimEllipsis(jsondata[i]["excerpt"])).addClass("excerpt");
           } else {
             readTime = $("<p></p>").text(jsondata[i]["readTime"] + " minutes");
+            //excerpt = $("<p></p>").text(jsondata[i]["excerpt"]).addClass("excerpt");
+            excerpt = $("<p></p>").text(trimEllipsis(jsondata[i]["excerpt"])).addClass("excerpt");
           }
         } else if (jsondata[i]["type"] == "shop") {
           readTime = $("<p></p>").text("Shopping Link");
@@ -63,6 +83,7 @@ $(document).ready(function(){
         // append all
         articleDiv.append(headline[0]);
         articleDiv.append(readTime[0]);
+        articleDiv.append(excerpt[0]);
         articleContainer.append(articleDiv);
       }
     });
